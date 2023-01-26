46596040fb5e6af9db7add098e983a8bbdb1fe5b
Colin Lieberman <colinlieberman@gmail.com>
2023-01-25 18:23:40 -0800

    dont blow up submit buttons

--- app/javascript/notes.js
+++ app/javascript/notes.js
@@ -7,12 +7,27 @@ const Notes = {
   },
 
   initNoteModal: function() {
+    const that = this;
+    this.modalCancelButton.on('click', function(e) {
+      e.preventDefault();
+      that.noteModal.hide();
+      that.noteModal.find('input,textarea').val(function() {
+        if(this.type == 'submit') {
+          return this.value;
+        }
+        else {
+          return '';
+        }
+      });
+      that.noteModal.find('input,textarea').data('value', '');
+    });
   },
 
   init: function() {
     this.notes = $('#notes tbody tr');
     this.noteModal = $('#note-modal');
     this.addNoteButtons = $('.add-note');
+    this.modalCancelButton = this.noteModal.find('button.cancel');
 
     this.initAddNoteButtons();
     this.initNoteModal();
